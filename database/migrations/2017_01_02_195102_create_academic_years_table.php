bcf7db451739fb1f3697053b6453cd06beddafbf
My Peecee <My Peecee@peecee>
2017-01-22 04:30:55 +0600

    almost all done except mark entry, hope it will be done at the morning...

---
+++ database/migrations/2017_01_02_195102_create_academic_years_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAcademicYearsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academic_years', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('academic_year', 255 );
+            
+            $table->string('created_by', 255 )->default('user_from_session');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academic_years');
+    }
+}
